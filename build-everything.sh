eb8e224237678a96a096c9a0d4ea07e504d7c3c1
Kyle Maxwell <kyle@kylemaxwell.com>
2011-06-13 12:15:01 -0700

    retrieve articles

--- build-everything.sh
+++ build-everything.sh
@@ -1,15 +1,26 @@
 #!/bin/sh
 cd `dirname $0`/bench
-if [ ! -d data ]
+if [ ! -d data/names ]
 then
-  mkdir -p data
-  wget -O data/names.bz2 http://download.freebase.com/wex/2011-05-13/freebase-wex-2011-05-13-freebase_names.tsv.bz2
-  cd data
+  mkdir -p data/names
+  wget -O data/names/names.bz2 http://download.freebase.com/wex/2011-05-13/freebase-wex-2011-05-13-freebase_names.tsv.bz2
+  cd data/names
   bunzip2 names.bz2
   split -a 6 -l 10000 names
   rm names
   cd -
 fi
+
+if [ ! -d data/articles ]
+then
+  mkdir -p data/articles
+  cd data/articles
+  curl http://download.freebase.com/wex/latest/freebase-wex-2011-05-27-articles.tsv.bz2 | bzcat | cut -f 1 -f 4 | sed -E 's/<[^>]*>/ /g' | sed 's/\\n/ /g' | head -500000 > articles
+  split -a 6 -l 1000 articles
+  rm articles
+  cd -
+fi
+
 if [ ! -d solr ]
 then
   mkdir -p solr
