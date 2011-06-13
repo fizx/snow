4ef937cf9381dd8314198297b3f8b7bc1573bce2
Kyle Maxwell <kyle@kylemaxwell.com>
2011-06-13 10:50:55 -0700

    build xml

--- build-everything.sh
+++ build-everything.sh
@@ -28,4 +28,5 @@ cp ./target/scala_2.8.1/snow_2.8.1-0.1.jar WEB-INF/lib
 zip bench/solr/apache-solr-3.2.0-nrt/example/webapps/solr.war WEB-INF/lib/*
 rm -rf WEB-INF
 cd bench
-cp misc/solrconfig-nrt.xml solr/apache-solr-3.2.0-nrt/example/solr/conf
+cp misc/solrconfig-nocache.xml solr/apache-solr-3.2.0/example/solr/conf/solrconfig.xml
+cp misc/solrconfig-nrt.xml solr/apache-solr-3.2.0-nrt/example/solr/conf/solrconfig.xml
