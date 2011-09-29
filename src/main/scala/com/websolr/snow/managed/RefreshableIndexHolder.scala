71859bac59e7b5f3a9fd589c0a66c0dc275466c8
Kyle Maxwell <kyle@kylemaxwell.com>
2011-09-28 19:10:33 -0700

    make replication possible

--- src/main/scala/com/websolr/snow/managed/RefreshableIndexHolder.scala
+++ src/main/scala/com/websolr/snow/managed/RefreshableIndexHolder.scala
@@ -3,4 +3,5 @@ package org.apache.lucene.index
 class RefreshableIndexHolder(ir: IndexReader) extends FilterIndexReader(ir) {
   def setInternal(i: IndexReader) = this.in = i
   override def getSequentialSubReaders() = null
+  override def getIndexCommit() = IndexReader.open(this.in.directory, true).getIndexCommit()
 }
